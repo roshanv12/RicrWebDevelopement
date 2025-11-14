9778833b0c03258fffad716033fccbedcf10930a
roshanv12 <rv7437764@gmail.com>
2025-11-14 22:57:30 +0530

    javasriptFirstClass

---
+++ JavaScript/basicJS/index.js
@@ -0,0 +1,17 @@
+var a = 10;
+
+var a = 20;
+
+let b = 10;
+
+console.log(a,b);
+
+let ar = ["abc",45.67,'a',true]
+
+console.warn("this is warning")
+
+console.warn("ye error hai")
+
+alert("this is alert")
+
+if (confirm("are you "))
