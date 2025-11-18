2f218c19dda0f8252b38c9e8c49b7ca38a71ad63
roshanv12 <rv7437764@gmail.com>
2025-11-18 14:16:59 +0530

    Login&Registration page in bootstrap

---
+++ Project02/loginpageCSS/index.js
@@ -0,0 +1,16 @@
+function login() {
+    console.log("Login Button Clicked");
+
+   const em = document.getElementById("LoginEamil").value;
+   console.log(em);
+
+
+
+   
+
+}
+
+function Registration() {
+    console.log("Registration Button Clicked");
+    
+}
