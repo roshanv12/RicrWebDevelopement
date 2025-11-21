7cdd085feed62acb828986cad531b8da019dbde1
roshanv12 <rv7437764@gmail.com>
2025-11-21 18:16:25 +0530

    javascriptBasic

---
+++ JavaScript/basicJS/index.js
@@ -0,0 +1,25 @@
+console.log("Roshan vishwakarma");
+console.log("This is my first js code");
+console.log("Welcome to my web page");
+console.log("RICR Student");
+
+// FullName = "Roshan Vishwakarma";
+// age = 23;
+// price = 999;
+// x = null;
+// y = undefined;
+// console.log(age);
+
+
+let fullName = "Roshan Vishwakarma";
+let age = 24;
+let totalPrice = 99.99;
+
+console.log( "age = " + age);
+console.log( "Fullname = " + fullName);
+console.log("Total Price = " + totalPrice);
+
+
+let a;
+a = 50;
+console.log(a)
---
+++ JavaScript/basicJS/index01.js
@@ -0,0 +1,36 @@
+// Operator & Conditional Statement
+
+// Artimetic Operators
+
+// let a = 9;
+// let b = 4;
+
+// console.log("a =", a, " & b =", b)
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b); 
+
+// Unary Operator
+
+// let a = 9;
+// let b = 4;
+
+// console.log("a =", a, " & b =", b)
+// a++;    /* 6 */
+// b--;    /* 3 */
+// console.log(a);
+// console.log(b);
+
+
+// Asignment Operators
+
+let a = 9;
+let b = 4;
+
+a += 4;
+console.log("a = ", a);
+
+
