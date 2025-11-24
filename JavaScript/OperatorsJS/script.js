fd755e27f6da692fdfb72f7c8908417a3c312dfe
roshanv12 <rv7437764@gmail.com>
2025-11-24 23:11:18 +0530

    operator

---
+++ JavaScript/OperatorsJS/script.js
@@ -0,0 +1,38 @@
+/*** Operators in JS ***/
+
+/** Arithmetic Operators => (+ , - , * , / , % ) **/
+
+let a = 5;
+let b = 2;
+
+let c = a+b;
+let c1 = a-b;
+let c2 = a*b;
+let c3 = a/b;
+let c4 = a%b;
+
+console.log("a =", a , "b =", b );
+
+console.log("a + b = ", c );
+console.log("a - b = ", c1);
+console.log("a * b = ", c2);
+console.log("a / b = ", c3);
+console.log("a % b = ", c4);
+
+/* Unary Operators */
+
+let x = 6;
+let y = 3;
+
+console.log("x =", x , "y =", y);
+// x++; 
+// y--;
+// console.log("x++ =", x); // 7
+// console.log("y-- =", y); // 2
+
+console.log("x++ =", x++);
+console.log("x = ", x)
+
+
+
+
