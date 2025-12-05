4c295165c8bce2f0dfe137756c749fe845a6ab4d
roshanv12 <rv7437764@gmail.com>
2025-12-05 13:24:26 +0530

    DOM

---
+++ JavaScript/DOM/script.js
@@ -0,0 +1,15 @@
+// let paraElement = document.getElementById('spara');
+// console.log(paraElement);
+
+// console.log(paraElement.style);
+
+// paraElement.style.backgroundColor = "blue";
+
+let firstElement = document.querySelector("#fdiv");
+console.log(firstElement);
+
+/**class add by setAttribute*/
+
+firstElement.setAttribute("class","secondDivClass");
+
+console.log(firstElement);
---
+++ JavaScript/functionsJS/script01.js
@@ -0,0 +1,37 @@
+// console.log('roshan');
+
+// console.log(Math.PI);
+
+// console.log(Math.max(60,40,745,23,12));
+
+// console.log( Math.min(12,34,54,65,76,57,98,9));
+
+// console.log(Math.round(1.8));
+
+// console.log(Math.ceil(1.9));
+
+// console.log(Math.floor(1.9));
+
+// console.log(Math.abs(-12));
+
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+
+/** Object cloning && Garbage collector  **/
+
+
+let src = {
+    age:12,
+    wt:50,
+    ht:180
+
+};
+
+let dest = {...src};
+
+console.log("src: ", src);
+console.log("dest: ", dest);
+
+
