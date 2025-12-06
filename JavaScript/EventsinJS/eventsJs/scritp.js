94a28d631b991aef171f7b225d8418d2e95daf35
roshanv12 <rv7437764@gmail.com>
2025-12-06 13:19:11 +0530

    EventsJs

---
+++ JavaScript/EventsinJS/eventsJs/scritp.js
@@ -0,0 +1,25 @@
+/*** addEventListener ****/
+// let fpara = document.getElementById("fpara");
+//  fpara.textContent = "Hello Roshan";
+
+// function changeText() {
+    
+// }
+
+// let fpara = document.getElementById("fpara");
+// fpara.addEventListener('click',changeText);
+
+/*** removeEventListener ****/
+// fpara.removeEventListener('click',changeText);
+
+
+ 
+
+function changeText() {
+    let fpara = document.getElementById("fpara");
+    fpara.textContent = "hello RoshanVishwakarma";
+}
+ let fpara = document.getElementById("fpara");
+
+ fpara.addEventListener('click', changeText);
+
--- JavaScript/EventsinJS/script.js
+++ JavaScript/EventsinJS/script.js
@@ -1,45 +1,87 @@
-function on() {
-    document.getElementById("bulb").style.backgroundColor = "yellow";
-}
 
-function off() {
-    document.getElementById("bulb").style.backgroundColor = "white";
+function On() {
+  document.getElementById("bulb").style.backgroundColor = "yellow";
 }
-
-function onred() {
-    document.getElementById("bulb").style.backgroundColor = "red";
-}
-
-function ongreen() {
-    document.getElementById("bulb").style.backgroundColor = "green";
-}
-
-function onblue() {
-    document.getElementById("bulb").style.backgroundColor = "blue";
+function Off() {
+  document.getElementById("bulb").style.backgroundColor = "white";
 }
 
 const userColor = document.getElementById("color");
 
-userColor.addEventListener("change", () => changeBulbColor(userColor.value));
+userColor.addEventListener("change", () => changeBulbColour(userColor.value));
 
-
-function changeBulbColor(color) {
-    document.getElementById("bulb").style.backgroundColor = color;
+function changeBulbColour(color) {
+  document.getElementById("bulb").style.backgroundColor = color;
 }
 
 function SB_Control() {
-    const btn = document.getElementById("SB_btn");
-    if (btn.innerText === "on") {
-        document.getElementById("SB_btn").innerText = "Off";
-        document.getElementById("smartBulb").classList.add("on");
-
-    }else {
-        document.getElementById("SB_btn").innerText = "On";
-        document.getElementById("smartBulb").classList.remove("on");
-    }
+  const btn = document.getElementById("SB_btn");
+  if (btn.innerText === "On") {
+    document.getElementById("SB_btn").innerText = "Off";
+    document.getElementById("smartBulb").classList.add("on");
+  } else {
+    document.getElementById("SB_btn").innerText = "On";
+    document.getElementById("smartBulb").classList.remove("on");
+  }
 }
 
 function SB_Control2() {
-    
+  document.getElementById("smartBulb").classList.toggle("on");
 }
 
+document.getElementById("c1").addEventListener("mouseenter", () => {
+  fillColour("violet");
+});
+
+document.getElementById("c1").addEventListener("mouseleave", () => {
+  fillColour("white");
+});
+
+function fillColour(Color) {
+  console.log(Color);
+  document.getElementById("rainbowbulb").style.backgroundColor = Color;
+}
+
+document.getElementById("c2").addEventListener("mouseenter", () => {
+  fillColour("indigo");
+});
+
+document.getElementById("c2").addEventListener("mouseleave", () => {
+  fillColour("white");
+});
+document.getElementById("c3").addEventListener("mouseenter", () => {
+  fillColour("blue");
+});
+
+document.getElementById("c3").addEventListener("mouseleave", () => {
+  fillColour("white");
+});
+
+document.getElementById("c4").addEventListener("mouseenter", () => {
+  fillColour("green");
+});
+
+document.getElementById("c4").addEventListener("mouseleave", () => {
+  fillColour("white");
+});
+document.getElementById("c5").addEventListener("mouseenter", () => {
+  fillColour("yellow");
+});
+
+document.getElementById("c5").addEventListener("mouseleave", () => {
+  fillColour("white");
+});
+document.getElementById("c6").addEventListener("mouseenter", () => {
+  fillColour("orange");
+});
+
+document.getElementById("c6").addEventListener("mouseleave", () => {
+  fillColour("white");
+});
+document.getElementById("c7").addEventListener("mouseenter", () => {
+  fillColour("red");
+});
+
+document.getElementById("c7").addEventListener("mouseleave", () => {
+  fillColour("white");
+});
