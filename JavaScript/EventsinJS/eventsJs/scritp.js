74bcd9f4093708ef2de3640a9a5deae87d013136
roshanv12 <rv7437764@gmail.com>
2025-12-07 10:16:46 +0530

    eventsJs

--- JavaScript/EventsinJS/eventsJs/scritp.js
+++ JavaScript/EventsinJS/eventsJs/scritp.js
@@ -1,25 +1,32 @@
-/*** addEventListener ****/
-// let fpara = document.getElementById("fpara");
-//  fpara.textContent = "Hello Roshan";
+// //Code 1
 
-// function changeText() {
-    
+// const t1 = performance.now();
+
+// for(let i = 0; i<=100; i++) {
+//     let para = document.createElement("p");
+//     para.textContent = "This is My Para" + i;
+//     document.body.appendChild(para);
 // }
 
-// let fpara = document.getElementById("fpara");
-// fpara.addEventListener('click',changeText);
+// const t2 = performance.now();
 
-/*** removeEventListener ****/
-// fpara.removeEventListener('click',changeText);
+// console.log("total time by code 1:" + (t2-t1));
+
+// // code  2
+
+// let myDiv = document.createElement("div");
+// div.textContent = "This is My div"
 
 
- 
+let fragment = document.createDocumentFragment();
 
-function changeText() {
-    let fpara = document.getElementById("fpara");
-    fpara.textContent = "hello RoshanVishwakarma";
+for(let i = 1; i<=100; i++) {
+    let para = document.createElement("p");
+    para.textContent = "This is My first Para" + i;
+    fragment.appendChild(para);
+
 }
- let fpara = document.getElementById("fpara");
 
- fpara.addEventListener('click', changeText);
+document.body.appendChild(fragment);
+
 
