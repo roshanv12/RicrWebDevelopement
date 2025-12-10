ef3b429876488e19147c1d481cd4b4cdbe12b5c7
roshanv12 <rv7437764@gmail.com>
2025-12-10 22:10:31 +0530

    baisicJS

---
+++ JavaScript/basicJS/functionjs/script.js
@@ -0,0 +1,77 @@
+
+
+//fu
+
+function roshan(){
+    console.log("RICR");
+}
+
+roshan();
+
+// function declarations
+
+
+ let RICRstudent = function(){
+    console.log("RICR");
+}
+
+RICRstudent();
+
+// Arrow functions
+
+let fnc = () =>{
+    console.log("RICR");
+}
+
+fnc()
+
+
+function roshan1(a) {
+    console.log(a);
+
+}
+
+let abcd = function(){
+    console.log("roshan");
+}
+
+let a = ()=>{
+    console.log("roshan");
+}
+
+roshan1("roshan1243");
+abcd();
+a();
+
+// function UserName(a){
+//     console.log(`My name is ${a}`);
+// }
+
+// UserName("Roshni")
+// UserName("rounak")
+// UserName("sadhna patel")
+// UserName("piyush chourey")
+// UserName("Arun sarathe")
+// UserName("Prince")
+// UserName("Aman")
+// UserName("Amit patel")
+
+function userName(name){
+    console.log(name)
+}
+
+userName("RICRstudent");
+
+function abcd2(a,b,c,d,f,...num) {
+    console.log(a,b,c,d,f,num);
+}
+
+abcd2(1,2,3,4,5,6,7,8,9,10);
+
+// function roshan(NUM){
+//     console.log(NUM);
+//    return "roshanRICR";
+// }
+
+//  let a = roshan(12);
+//  console.log(a);
---
+++ JavaScript/basicJS/fundamentalJs/script.js
@@ -0,0 +1,22 @@
+// var , let , const (veriable creating tools)
+
+// var a;
+// var a = 12;
+// let a = 23; // let ki value ham change kar sakte hai.
+// const a = 43; // const ki value kanhi change nhi hoti hai.
+
+// // declaration & Initialisation in js
+
+// var a ; // decleare karna
+// var a = 12; // declare and initialize karna.
+
+
+var a = 12; 
+var a = 34;
+// var window mein add hota hai.
+// function scoped hota hai.
+// aap firse declar kr skte he same name se error nahi aayega.
+
+
+
+
