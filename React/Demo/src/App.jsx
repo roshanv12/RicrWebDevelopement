b61a1bdd5cb383b5dbd778c55d60c0f01be87e27
roshanv12 <rv7437764@gmail.com>
2025-12-26 21:53:31 +0530

    React

--- React/Demo/src/App.jsx
+++ React/Demo/src/App.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import contact from "./pages/Contact";
+import About from "./pages/Product";
 
 function App() {
-  let a = 5;
-  let b = 10;
-  console.log(a + b);
   return (
     <>
-      <h1 id="head">Hello World</h1>
-      <div>this is my first React App</div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
-
-
 
 export default App;
