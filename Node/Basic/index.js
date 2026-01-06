e2c7b8efdffc275c8d43ea3671d6291b1b4efc13
roshanv12 <rv7437764@gmail.com>
2026-01-06 20:38:04 +0530

    chore: initialize package.json for firstBackend with basic configuration and dependencies

---
+++ Node/Basic/index.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+  console.log(a + b);
+}
+function sub(a, b) {
+  console.log(a - b);
+}
+
+console.log("abcd");
+
+// export { add, sub };
+export default mul;
---
+++ Node/Basic/index2.js
@@ -0,0 +1,10 @@
+// import { add } from "./index.js";
+// import kuchBhi from "./index.js";
+import add from "./index.js";
+
+let x = 10;
+let y = 20;
+
+add(x, y);
+// sub(y, x);
+// kuchBhi(x, y);
