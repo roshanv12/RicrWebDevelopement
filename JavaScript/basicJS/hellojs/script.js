d05429a862368f2b389a415d0561d04a8b655764
roshanv12 <rv7437764@gmail.com>
2026-01-07 22:20:49 +0530

    refactor: update script.js to demonstrate data types and string usage

--- JavaScript/basicJS/hellojs/script.js
+++ JavaScript/basicJS/hellojs/script.js
@@ -1,1 +1,9 @@
-console.log("roshan vishwakarma");
+console.log(typeof 23);
+console.log(typeof -4.0054545);
+// strings data types:- ("one line or charactor", 'one line or charac', `multi lines of strings`)
+
+console.log("Roshan");
+console.log(typeof"Roshan Vishwakarma");
+console.log(`lorem
+    hello roshan
+    `)
