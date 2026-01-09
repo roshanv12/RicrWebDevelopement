abc18fc7a8a64396feb5bedd5b9c74250f5f3bc2
roshanv12 <rv7437764@gmail.com>
2026-01-09 21:14:09 +0530

    feat: add user authentication with registration and login functionality
    - Implemented user registration and login endpoints in myRouter.js
    - Created User model for MongoDB with necessary fields
    - Added controller functions for user registration, login, and logout
    - Established MongoDB connection in db.js
    - Updated index.js to include routes and middleware
    - Added mongoose as a dependency for MongoDB interactions
    - Configured nodemon for development environment

--- JavaScript/basicJS/hellojs/script2.js
+++ JavaScript/basicJS/hellojs/script2.js
@@ -41,6 +41,56 @@ console.log(message.toLowerCase().trimEnd().trimStart());
 console.log(message.toUpperCase().trimStart());
 
 let collageStudents = "    Roshan Vishwakamra   ";
-let resultCollage = collageStudents.trim().toLowerCase()
- console.log(resultCollage.toUpperCase());
- 
+let resultCollage = collageStudents.trim().toLowerCase();
+console.log(resultCollage.toUpperCase());
+
+// Mthods (with Arguments) :- 1] includes, 2] indexOf, 3] replace() & replaceAll(), 4] concat(),
+// 5] padStart() & padEnd(), 6] charAt() & charCodeAt(), 7] split()
+
+//includes() // ham chek karte hai ye hamari String me he ya nhi ager hai to true result deta hai
+// ager nhi hai to ye hame false result deta hai.
+const introduction = "   Hi, I am Roshan  Vishwakarma";
+console.log(introduction.trim().toUpperCase());
+console.log(introduction.includes("Hi,")); // true
+console.log(introduction.includes("A")); // false
+console.log(introduction.includes("roshan")); // false
+
+// indexOf(): hamre jo v character ya string hai wo  kon se index par exist karta hai.
+console.log(introduction.indexOf("n"));
+console.log(introduction.indexOf("am"));
+console.log(introduction.indexOf("V"));
+
+// replace(): kisi v character ko replace karke uski jagah kisi or character ya string ko likhna
+
+console.log(introduction.replace("Hi", "Hello"));
+console.log(introduction.replace("I", "My name is"));
+console.log(introduction.replaceAll("a", "A"));
+
+const introduction1 = "   Hello, Vishwakarma ji";
+let result5 = introduction + "       " + introduction1;
+console.log(result5.trim().toUpperCase());
+
+//concat: do strings ko add karne ke liye use karte hai
+
+// const introduction1 = "   Hello, Vishwakarma ji";
+// console.log(introduction.replace("Hi",'Hello'))
+
+console.log(introduction.concat(" hello", "Hello world", introduction1));
+
+// patStart(): bank account nomber , creaded card PIN cteat karne ke liye use karte hai.
+
+const accountNo = "5609"; // last for disit.
+const accountNoCreat = accountNo.padStart(12, "X");
+const accountNoCreat1 = accountNo.padStart(16, "#");
+const accountNoCreat2 = accountNo.padStart(16, "*");
+
+console.log(accountNoCreat); // output: xxxxxxxx5609
+console.log(accountNoCreat1); // output: ############5609
+console.log(accountNoCreat2); //************5609
+
+let newAccountNo = `Last for digit in My Account no. ${accountNo} Hello Js ${introduction1.trimStart()}`;
+console.log(newAccountNo.toLowerCase());
+console.log(newAccountNo.toUpperCase());
+
+
+
--- Node/firstBackend/index.js
+++ Node/firstBackend/index.js
@@ -2,8 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import connectDB from "./src/config/db.js";
+import AuthRouter from "./src/routers/myRouter.js";
 
 const app = express();
+
+app.use(express.json());
+app.use("/auth", AuthRouter);
+
 app.get("/", (req, res) => {
   console.log("Server is Running");
   res.json({ message: "Server is Running Sucessfully" });
@@ -12,4 +18,5 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server Started at port", port);
+  connectDB();
 });
