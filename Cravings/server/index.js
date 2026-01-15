a1d2e21435f06a5bc853c4d9e35ea765e2ec6036
roshanv12 <rv7437764@gmail.com>
2026-01-15 23:47:05 +0530

    feat: implement contact form with validation, submission handling, and API integration

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -2,20 +2,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import connectDB from "./src/config/db.js";
 import AuthRouter from "./src/routers/authRouter.js";
+import publicRouter from "./src/routers/publicRouter.js";
 import morgan from "morgan";
 
 const app = express();
 
-app.use(cors({
-  origin:"http://localhost:5173"
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 app.use("/auth", AuthRouter);
+app.use("/public", "PublicRouter");
 
 app.get("/", (req, res) => {
   console.log("Server is Working");
---
+++ Cravings/server/src/controllers/publicController.js
@@ -0,0 +1,31 @@
+export const NewContact = async (req, res, next) => {
+  try {
+    const { fullName, email, mobile, message } = req.body;
+
+    if (!fullName || !email || !mobile || !message) {
+      const error = new Error("All feilds required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    //save data to database
+
+    const newContact = await Contact.create({
+      fullName,
+      email,
+      mobileNumber,
+      message,
+    });
+
+    console.log(newContact);
+
+    res
+      .status(201)
+      .json({
+        message:
+          "Thanks for contactiong us. we will get Back to you in 24-48 Hours",
+      });
+  } catch (error) {
+    next(error);
+  }
+};
