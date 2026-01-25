ce443b9872f73314541e6edf382fc05ffd773bc9
roshanv12 <rv7437764@gmail.com>
2026-01-25 21:42:45 +0530

    feat: add multer for file uploads and update userRouter to handle file uploads in user update

--- Cravings/server/src/routers/userRouter.js
+++ Cravings/server/src/routers/userRouter.js
@@ -3,7 +3,8 @@ import { UserUpdate } from "../controllers/userController.js";
 import { Protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
+const uploads = 
 
-router.put("/update", Protect, UserUpdate);
+router.put("/update", Protect,uploads.single() UserUpdate);
 
 export default router;
