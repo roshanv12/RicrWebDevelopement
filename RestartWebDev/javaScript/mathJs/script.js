0fa170c0a2e0a73e6bc7961bfb41700b4c0e06f4
roshanv12 <rv7437764@gmail.com>
2026-06-25 23:21:15 +0530

    Refactor code structure for improved readability and maintainability

---
+++ RestartWebDev/javaScript/mathJs/script.js
@@ -0,0 +1,61 @@
+const a = Math.PI;
+console.log(a);
+
+const b = Math.SQRT2;
+console.log(b);
+
+let num = 90 ** 2;
+console.log(num);
+
+const sqrtNum = Math.sqrt(100);
+console.log(sqrtNum);
+
+const powResult = Math.pow(19, 2);
+console.log(powResult);
+
+// let r = alert("Are you true");
+
+// const confirmresult = confirm("Hello!");
+// console.log(confirmresult);
+
+// const promptResult = prompt("Please enter your naem".toUpperCase())
+// console.log(promptResult.toLowerCase())
+
+const floorResult = Math.floor(35.5454);
+console.log(floorResult);
+
+const ceilResult = Math.ceil(12.6);
+console.log(ceilResult);
+
+const ceilResult2 = Math.ceil(-4.43);
+console.log(ceilResult2);
+
+const roundResult = Math.round(2.5);
+console.log(roundResult);
+
+const roundResult2 = Math.round(-2.8);
+console.log(roundResult2);
+
+const randomResult = Math.floor(Math.random() * 10);
+// console.log(Math.round(randomResult));
+console.log(randomResult);
+
+const randomNum = Math.floor(Math.random() * 11) + 10;
+console.log(randomNum);
+
+const c1 = Boolean(0);
+console.log(c1);
+
+const c2 = Boolean(NaN);
+console.log(c2);
+
+const c3 = Boolean(undefined);
+console.log(c3);
+
+const c4 = Boolean("");
+console.log(c4);
+
+const c5 = Boolean(" ");
+console.log(c5);
+const c6 = Boolean(-0);
+console.log(c6);
---
+++ RestartWebDev/javaScript/strings/script.js
@@ -0,0 +1,7 @@
+const userName = '     My name is Roshan Vishwakarma        '
+
+console.log(userName)
+
+let a = userName.toLocaleLowerCase().toLocaleUpperCase().trimStart().trimEnd()
+
+console.log(a)
