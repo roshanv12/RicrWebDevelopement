2efe4914df45caa22358579dce537c1fa68418dc
roshanv12 <rv7437764@gmail.com>
2026-06-29 11:17:19 +0530

    Add initial HTML and JavaScript files for objectJs and ternaryOperator examples

---
+++ RestartWebDev/javaScript/objectJs/script.js
@@ -0,0 +1,10 @@
+const userInformetion = {
+  userName: "Roshan vishwakarma",
+  collage: "SIRT Bhopal",
+  age: 20,
+  email: "roshan@gmail.com",
+  contectNumber: 7999687475,
+};
+
+
+console.log(userInformetion)
---
+++ RestartWebDev/javaScript/operators/ternaryOperator/script.js
@@ -0,0 +1,9 @@
+debugger
+
+const studentAge = prompt("Please inter your age?");
+const result =
+  studentAge >= 18 ? "You are collage student" : "You are school student";
+console.log(result);
+
+const adultResult = studentAge >= 18 ? "You are Adult" : "You are not adult";
+console.log(adultResult);
