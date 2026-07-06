b73098f75057ca21ee3fda46a937f98300bb3c7a
roshanv12 <rv7437764@gmail.com>
2026-07-06 22:16:07 +0530

    Add HTML and JavaScript files for Arrow Functions, Event Loop and Callback Queue, Returning Functions, For Each, and For of Loop concepts

---
+++ RestartWebDev/javaScript/loopsInJavacript/forEach/script.js
@@ -0,0 +1,13 @@
+const fruits = ["banana", "apple", "peach", "mango", "grapes"];
+
+fruits.forEach((values) => {
+  console.log(values);
+});
+
+// fruits.forEach((friends) => {
+//   console.log(fruits);
+// });
+
+fruits.forEach((key) => {
+  console.log(key);
+});
