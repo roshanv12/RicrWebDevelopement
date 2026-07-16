f22adc86922f8cfe1c6da306762a3e730a1b4c9c
roshanv12 <rv7437764@gmail.com>
2026-07-16 20:55:10 +0530

    Add initial HTML structure and CSS styles for IMG-GOAL project
    - Created index.html with basic HTML structure and a main container for the image.
    - Added style.css to define styles for the main container and image.
    - Included a placeholder image in the HTML file.
    - Set up basic styling to ensure proper layout and appearance.

--- RestartWebDev/projectAll/javaScriptProjects/firstJavaScriptProject/script.js
+++ RestartWebDev/projectAll/javaScriptProjects/firstJavaScriptProject/script.js
@@ -1,1 +1,41 @@
-console.log("First JavaScript Project")
+// const checkBoxList = document.querySelectorAll(".custom-checkBox");
+
+// const inputFields = document.querySelectorAll(".goal-input")
+
+// checkBoxList.forEach((checkBox) => {
+//   checkBox.addEventListener("click", () => {
+//     const allFieldsFilled = [...inputFields].every(function (input) {
+//       return input.value;
+//     });
+
+//     const icon = checkBox.querySelector("i");
+//     icon.classList.toggle("show");
+//     const input = checkBox.parentElement.querySelector(".goal-input");
+//     input.classList.toggle("completed");
+//   });
+// });
+
+const checkBoxList = document.querySelectorAll(".custom-checkBox");
+const inputFields = document.querySelectorAll(".goal-input");
+const error = document.querySelector(".error");
+
+checkBoxList.forEach((checkBox) => {
+  checkBox.addEventListener("click", () => {
+    const allFieldsFilled = [...inputFields].every((input) => {
+      return input.value.trim() !== "";
+    });
+
+    if (!allFieldsFilled) {
+      error.style.display = "block";
+      return;
+    }
+
+    error.style.display = "none";
+
+    const icon = checkBox.querySelector("i");
+    icon.classList.toggle("show");
+
+    const input = checkBox.parentElement.querySelector(".goal-input");
+    input.classList.toggle("completed");
+  });
+});
