1bc101638858b304fcd678852f44cc39cc247278
roshanv12 <rv7437764@gmail.com>
2026-07-19 20:25:29 +0530

    Add Love Calculator project with HTML, CSS, and JavaScript functionality

---
+++ RestartWebDev/projectAll/javaScriptProjects/loveCalculator/script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector(".form-container");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const boy = document.querySelector(".inputBoy").value.length;
+  const girl = document.querySelector(".inputGirl").value.length;
+
+  console.log(boy);
+  console.log(girl);
+
+  const result = Math.pow(boy + girl, 3) % 101;
+  console.log(result);
+
+  document.querySelector("h2").innerText = `Result: ${result}%`;
+});
