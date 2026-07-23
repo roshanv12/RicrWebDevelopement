a4d7852175e6b9b19afb3acaf10af9a402a5aeae
roshanv12 <rv7437764@gmail.com>
2026-07-23 22:51:40 +0530

    Add initial HTML and JavaScript files for array methods demonstration

---
+++ RestartWebDev/javaScript/arrayMethods/script01.js
@@ -0,0 +1,28 @@
+// const arr = ["Roshan", "Prince", "Piyush", "Nikhil", "Arun", "Nitin"];
+
+// // console.log(arr);
+
+// // arr.forEach((e)=> {
+// //     console.log(e)
+// // })
+
+// const userInformation = {
+//   userName: "roshan",
+//   age: 20,
+//   city: "itarsi M.P",
+//   branch: "csbs",
+// };
+
+// // console.log(userInformation);
+
+// userInformation.map((e)=> {
+//     console.log(e)
+// })
+
+const arr = ["Roshan", "Prince", "Piyush", "Nikhil", "Arun", "Nitin"];
+
+console.log(arr);
+
+arr.forEach((arr, index) => {
+  console.log(index ,arr);
+});
